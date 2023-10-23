906d21e2a9c6be2f1955ea3f65ab881e27afa2fd
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-10-23 15:43:07 -0400

    Added an instance of NameFragment to MainActivity using a FragmentTransaction

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -11,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, NameFragment())
+            .commit()
+
     }
 }
