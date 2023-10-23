92a5a0b1d1dd8564988f4f2591465e2cad8ba8d4
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-10-23 15:41:20 -0400

    Created a fragment instance, NameFragment, that performs the functionality currently performed by MainActivity

--- app/src/main/java/edu/temple/fragmentrefactor/NameFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/NameFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class NameFragment : Fragment() {
 
@@ -13,7 +16,23 @@ class NameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_name, container, false)
+        val layout = inflater.inflate(R.layout.fragment_name, container, false)
+
+        val displayTextView = layout.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = layout.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = layout.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+
+        return layout
     }
 
 }
